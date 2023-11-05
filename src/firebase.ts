d4163e77e285d81141afa189c406840c59dc3893
Svaagi <jozefsvagerkom5@gmail.com>
2023-11-05 03:19:44 +0100

    env

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDGne7FcVbtCEm96T9RY5nRx5sfG7VLlro",
-  authDomain: "infomat-web.firebaseapp.com",
-  projectId: "infomat-web",
-  storageBucket: "infomat-web.appspot.com",
-  messagingSenderId: "292174046524",
-  appId: "1:292174046524:web:375f13889862fe511b6a39",
-  measurementId: "G-TFSDB6TGYC"
+  apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+  authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.GATSBY_FIREBASE_APP_ID,
+  measurementId: process.env.GATSBY_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
